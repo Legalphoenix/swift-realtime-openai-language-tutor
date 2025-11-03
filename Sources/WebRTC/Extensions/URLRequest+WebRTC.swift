beb440fc786f420fbc48b07920cb866bee992cb4
jonduvnjak <jonduvnjak@gmail.com>
2025-11-03 19:32:23 +0100

    Remove model query parameter from WebRTC call

--- Sources/WebRTC/Extensions/URLRequest+WebRTC.swift
+++ Sources/WebRTC/Extensions/URLRequest+WebRTC.swift
@@ -8,9 +8,7 @@ fileprivate let baseURL = URL(string: "https://api.openai.com/v1/realtime/calls"
 
 package extension URLRequest {
 	static func webRTCConnectionRequest(ephemeralKey: String, model: Model) -> URLRequest {
-		var request = URLRequest(url: baseURL.appending(queryItems: [
-			URLQueryItem(name: "model", value: model.rawValue),
-		]))
+		var request = URLRequest(url: baseURL)
 
 		request.httpMethod = "POST"
 		request.setValue("Bearer \(ephemeralKey)", forHTTPHeaderField: "Authorization")
